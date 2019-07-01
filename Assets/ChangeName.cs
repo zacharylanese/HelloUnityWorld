09e22296b7a6662666809b98f0a5da282c97237b
zacharylanese <52254752+zacharylanese@users.noreply.github.com>
2019-06-30 23:23:36 -0400

    Add files via upload

---
+++ Assets/ChangeName.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChangeName : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            TextMeshProUGUI textmeshPro = GetComponent<TextMeshProUGUI>();
+            textmeshPro.SetText("Hello World from Zach!");
+        }
+    }
+}
